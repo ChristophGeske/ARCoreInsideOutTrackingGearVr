685be96365fc9314785c4880e48de219c08c4deb
Christoph Geske <christophgeske2@googlemail.com>
2018-04-24 03:12:15 +0200

    Some setting changes to get stable framerates and a better scene added.

--- WhiteRoom/Assets/NewScripts/followARCoreCamera.cs
+++ WhiteRoom/Assets/NewScripts/followARCoreCamera.cs
@@ -6,9 +6,8 @@ using GoogleARCore;
 public class followARCoreCamera : MonoBehaviour {
 
 	public Transform target;
-	public float smoothSpeed = 0.5f; // can be between 0 and 1.
+	public float smoothSpeed = 0.5f; // can be between 0.0f and 1.0f.
 	public Vector3 posARCore; 
-	// test late update also for camera movement
 
 	void Start () {
 		posARCore = GoogleARCore.Frame.Pose.position;
@@ -21,8 +20,6 @@ public class followARCoreCamera : MonoBehaviour {
 		}else {
 			Vector3 desiredPosition = target.position;
 			Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed); 
-			//Vector3 smoothedPosition = target.position - transform.position; // point half way
-			//transform.position = target.position;
 			transform.position = smoothedPosition;
 		}
 		posARCore = GoogleARCore.Frame.Pose.position;
