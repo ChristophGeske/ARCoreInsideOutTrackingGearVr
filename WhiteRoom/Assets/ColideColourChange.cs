eed338b577266aca44a527ed3df90160beaf48df
Christoph Geske <christophgeske2@googlemail.com>
2018-04-24 23:57:42 +0200

    Added controller support and some gaming elements.
    Added controller support and some gaming elements like changing room colours.

---
+++ WhiteRoom/Assets/ColideColourChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColideColourChange : MonoBehaviour {
+
+	public Material newColour;
+	bool colourChangeCollision = false;
+
+	void Start (){
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(colourChangeCollision){
+			this.GetComponent<Renderer>().material = newColour;
+		}
+
+	}
+
+	void OnCollisionEnter(Collision other) {
+		colourChangeCollision = true;
+	}
+		
+}
