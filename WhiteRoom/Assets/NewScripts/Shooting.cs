55f311bbb538729b54929f4ebfbce37da7c90da0
Christoph Geske <christophgeske2@googlemail.com>
2018-04-29 22:30:29 +0200

    Added Sound. Simplefied Code.

---
+++ WhiteRoom/Assets/NewScripts/Shooting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+	public int counter = 0;
+	public AudioSource shootSound; 
+
+	// Use this for initialization
+	void Start () {
+		shootSound = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger)  ) // if trigger is pressed 
+		{
+			counter++;
+			if (counter % 13 == 0) { // and maximum every 13 frame 
+				Fire ();			// a bullet is released	
+				shootSound.Play();
+			}
+		}
+
+	}
+
+	void Fire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate (
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * -6; // -6 because forward should be backward
+
+		// Destroy the bullet after 1.5 seconds
+		Destroy(bullet, 1.5f);
+	}
+
+}
--- WhiteRoom/Assets/NewScripts/followARCoreCamera.cs
+++ WhiteRoom/Assets/NewScripts/followARCoreCamera.cs
@@ -3,26 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using GoogleARCore;
 
+
 public class followARCoreCamera : MonoBehaviour {
 
-	public Transform target;
+	public Transform ARCoreCamera;
 	public float smoothSpeed = 0.5f; // can be between 0.0f and 1.0f.
-	public Vector3 posARCore; 
+	public Vector3 StoredARCorePos; 
+	public Vector3 latestARCorePos;
 
 	void Start () {
-		posARCore = GoogleARCore.Frame.Pose.position;
+		StoredARCorePos = GoogleARCore.Frame.Pose.position;
 	}
-		
+
+	void Update () { 
+		latestARCorePos = GoogleARCore.Frame.Pose.position;
+		if (latestARCorePos.Equals(StoredARCorePos)) {
+			transform.position = ARCoreCamera.position;
+		}else {
+			Vector3 smoothedPosition = Vector3.Lerp (transform.position, ARCoreCamera.position, smoothSpeed); 
+			transform.position = smoothedPosition;
+		}
+		StoredARCorePos = GoogleARCore.Frame.Pose.position;
+	}
+
+}
+
+/*working but old*
+public class followARCoreCamera : MonoBehaviour {
+
+	public Transform ARCoreCamera;
+	public float smoothSpeed = 0.5f; // can be between 0.0f and 1.0f.
+	public Vector3 StoredARCorePos; 
+
+	void Start () {
+		StoredARCorePos = GoogleARCore.Frame.Pose.position;
+	}
+
 	void Update () { 
 
-		if (GoogleARCore.Frame.Pose.position.Equals(posARCore)) {
-			transform.position = target.position;
+		if (GoogleARCore.Frame.Pose.position.Equals(StoredARCorePos)) {
+			transform.position = ARCoreCamera.position;
 		}else {
-			Vector3 desiredPosition = target.position;
+			Vector3 desiredPosition = ARCoreCamera.position;
 			Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed); 
 			transform.position = smoothedPosition;
 		}
-		posARCore = GoogleARCore.Frame.Pose.position;
+		StoredARCorePos = GoogleARCore.Frame.Pose.position;
 	}
-		
+
 }
+*/
