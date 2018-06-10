c4e3b633619b1961c006eacdfe4c3d3be7698225
Christoph Geske <christophgeske2@googlemail.com>
2018-06-10 05:01:20 +0200

    Fixed standby issue when not using GearVR
    Fixed standby issue when not using GearVR.
    Also added new environment sounds.

---
+++ WhiteIsland/Assets/NewScripts/ScreenTimeoutOff.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenTimeoutOff : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep; // prevents Screen Timeout when not connected to GearVr for example when using in Daydream or Cardboard
+    }
+}
