b7022a9e40e4a5bf029254f2c22bc4eb5214ca55
Christoph Geske <christophgeske2@googlemail.com>
2018-06-30 21:50:56 +0200

    Added boundery detection system
    Displaying feature points when trigger button is pressed.
    Updated the ARCore SDK to 1.3.0

---
+++ WhiteIsland/Assets/NewScripts/BounderyController.cs
@@ -0,0 +1,30 @@
+﻿using GoogleARCore.Examples.Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounderyController : MonoBehaviour {
+
+    bool switchTruthValue = true;
+    int counter = 0;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<PointcloudVisualizer>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            counter++;
+            if (counter % 13 == 0)
+            { // hold for 13 frames to trigger effect 
+                switchTruthValue = !switchTruthValue;
+                GetComponent<PointcloudVisualizer>().enabled = switchTruthValue;    // enable/disable point cloud
+                GetComponent<MeshRenderer>().enabled = switchTruthValue;            // enable/disable point cloud
+            }
+        }
+	}
+}
