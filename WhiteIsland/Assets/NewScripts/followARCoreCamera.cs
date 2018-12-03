cb2bfe35089326c890bdb2f550148e1dc46b301b
Christoph Geske <christophgeske2@googlemail.com>
2018-12-03 08:09:40 +0100

    Some artistic changes and project clean up.

--- WhiteIsland/Assets/NewScripts/followARCoreCamera.cs
+++ WhiteIsland/Assets/NewScripts/followARCoreCamera.cs
@@ -11,10 +11,7 @@ public class FollowARCoreCamera : MonoBehaviour {
 	public Transform aRCoreCamera;
     public Transform userCamera;
 
-    Vector3 smoothedPosition; 
-    private Vector3 startPosition;
-    private Vector3 endPosition;
-    private float speed = 25.5f; //2.5 works also with some slow down
+    private float speed = 25.5f; //25.5 is a high value and in combination with lerp produces fast responsivnes with almost no lag. Lower values would produce more lag but also smooth out jittery movements when for examle bad tracking by the ARCoreCamera occures.
     private float maxSpeed;
 
     private Vector3 arCamPos;
@@ -22,30 +19,14 @@ public class FollowARCoreCamera : MonoBehaviour {
     private Vector3 camDist;
     private Vector3 halfCamDist;
     private Vector3 oldArCamPos;
-    private int counter = 0;
 
-    float smoothTime = 0.06f;
-    float yVelocity = 0.0f;
-
-    // 0.0159 is the distance the camera is away from the head. This leads to position changes when turning the had and we wanna avoid that. The current setting need still more refinement. 
-    private Vector3 headRotationCorrection = new Vector3(0,0,0.0159f);
-
-    /*
-    // the update method is twice as fast as the ARCore Camera. To avoid jittering we use Lerp to smooth out the movement at the start and the end of the movement.
-    void Update () {
-        startPosition = userCamera.transform.position; 
-        endPosition = aRCoreCamera.transform.position - headRotationCorrection;
-        maxSpeed = speed * Time.deltaTime;
-        // Using Lerp this way is a trick. TODO: There seem to be better solutions which should also be tested and might reduce lag further.
-        smoothedPosition = Vector3.Lerp(startPosition, endPosition, maxSpeed); 
-        
-        this.transform.position = smoothedPosition;                             
+    private void Start()
+    {
+        OVRPlugin.occlusionMesh = true; // recommendet by oculus not sure what the exact effects on performance are.
     }
-    */
 
     void Update()
-    {
-        
+    { 
         arCamPos = aRCoreCamera.transform.position;
         currentCamPos = userCamera.transform.position;
         camDist = arCamPos - currentCamPos;
@@ -53,30 +34,7 @@ public class FollowARCoreCamera : MonoBehaviour {
 
         maxSpeed = speed * Time.deltaTime;
 
-        /* // This solution works quite well wenn 60 fps can be reached
-        if (oldArCamPos == arCamPos)
-        {
-            userCamera.transform.position = arCamPos;
-        }
-        else // (oldArCamPos != arCamPos)
-        {
-            userCamera.transform.position = currentCamPos + halfCamDist;
-        }
-        */
-
-        /* // Works well but a small jittereing at the corners is noticable speed is 25
-        if (oldArCamPos == arCamPos)
-        {
-            userCamera.transform.position = Vector3.Lerp(currentCamPos, currentCamPos + halfCamDist, maxSpeed);
-        } else
-        {
-            userCamera.transform.position = Vector3.Lerp(currentCamPos, arCamPos, maxSpeed);
-        }
-        */
-
-
-
-        // Even the wrong one works well hope this one works great. Here we have the correct order. speed is 25. works perfect when 60fps are reached
+        // Even the wrong one works well hope this one works great. Here we have the correct order. speed is 25. works perfect when 60fps are reached.
         if (oldArCamPos == arCamPos)
         {
             userCamera.transform.position = Vector3.Lerp(currentCamPos, arCamPos, maxSpeed);
@@ -85,27 +43,7 @@ public class FollowARCoreCamera : MonoBehaviour {
         {
             userCamera.transform.position = Vector3.Lerp(currentCamPos, currentCamPos + halfCamDist, maxSpeed);
         }
-
-
-        /*
-        if (currentCamPos != arCamPos && counter == 0)
-        {
-            userCamera.transform.position = Vector3.Lerp(currentCamPos, currentCamPos + halfCamDist, speed); 
-            counter = 1;
-        }
-        else
-        {
-            if (currentCamPos != arCamPos && counter == 1)
-            {
-                counter = 0;
-                userCamera.transform.position = Vector3.Lerp(currentCamPos, arCamPos, speed);  
-            }
-        }
-        */
-
         oldArCamPos = aRCoreCamera.transform.position;
-
-        //this.transform.position = currentCamPos;
     }
     
 
